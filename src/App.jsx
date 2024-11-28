c66852bd860404c38d5817138d4f1a90ad784ebb
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-11-28 10:50:12 -0600

    Added conditional content

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,26 @@ import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
 import {useState} from "react";
+import {EXAMPLES} from "./data";
+
 function App() {
-    const [tab, setTab] = useState('Please click a button');
+    const [tab, setTab] = useState('');
     const handleSelect = (selectedButton) => {
         setTab(selectedButton);
     }
+
+    let tabContent = <p>Please select a topic.</p>;
+    if (tab)
+        tabContent = (<div id={'tab-content'}>
+
+            <h3>{EXAMPLES[tab].title}</h3>
+            <p>{EXAMPLES[tab].description}</p>
+            <pre>
+                <code>
+                    {EXAMPLES[tab].code}
+                </code>
+            </pre>
+        </div>)
 
     return (
         <div>
@@ -18,7 +33,8 @@ function App() {
                     <ul>
 
                         {CORE_CONCEPTS.map((item, index) => (
-                            <CoreConcept key={index} title = {item.title} description = {item.description} image = {item.image} />
+                            <CoreConcept key={index} title={item.title} description={item.description}
+                                         image={item.image}/>
                         ))}
                     </ul>
                 </section>
@@ -30,7 +46,7 @@ function App() {
                         <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
                         <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
                     </menu>
-                    {tab}
+                    {tabContent}
                 </section>
             </main>
         </div>
