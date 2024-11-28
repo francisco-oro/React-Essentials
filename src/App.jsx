9d29a05cd227ed2d200edf231768679858fbec3f
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-11-28 00:42:03 -0600

    Stored component styles needed for Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,18 @@
-import reactImg from "./assets/react-core-concepts.png"
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min
-}
-
-function Header() {
-    const description = reactDescriptions[getRandomInt(0,3)];
-
-    return (
-    <header>
-        <img src={reactImg} alt="Stylized atom"/>
-        <h1>React Essentials</h1>
-        <p>
-            {description} React concepts you will need for almost any app you are
-            going to build!
-        </p>
-    </header>
-)}
-
-function CoreConcept() {
-    return <li>
-        <img src="..." alt="..."/>
-        <h3>Title</h3>
-        <p>Description</p>
-    </li>
-}
-
+import {CORE_CONCEPTS} from "./data";
+import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
 function App() {
     return (
         <div>
-            <Header />
+            <Header/>
             <main>
                 <section id={'core-concepts'}>
                     <h2>Core Concepts</h2>
                     <ul>
-                        <CoreConcept />
+
+                        {CORE_CONCEPTS.map((item, index) => (
+                            <CoreConcept key={index} title = {item.title} description = {item.description} image = {item.image} />
+                        ))}
                     </ul>
                 </section>
 
