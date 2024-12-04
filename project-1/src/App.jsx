c1468e1b81f3d26b3f50ed1a7bbaff40ff3944c2
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-12-04 08:05:02 -0600

    Added game container to app.jsx

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
 
@@ -10,7 +11,7 @@ function App() {
                     <Player name={"Player 2"} symbol={"O"}/>
                 </ol>
 
-              GAME BOARD
+                <GameBoard />
             </div>
           LOG
         </main>
